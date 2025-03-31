14fd905d8bc16c44057e950cc54e73ba6f77b5d7
Bhupinder <bhup0006@alginquinlive.com>
2025-03-31 19:56:40 -0400

    added description to resource group,vnet and subnet in the output.tf

--- infra/tf-app/outputs.tf
+++ infra/tf-app/outputs.tf
@@ -1,3 +1,14 @@
 output "resource_group_name" {
-  value = azurerm_resource_group.app_rg.name
+  value       = azurerm_resource_group.app_rg.name
+  description = "The name of the Azure Resource Group."
+}
+
+output "virtual_network_name" {
+  value       = azurerm_virtual_network.vnet.name
+  description = "The name of the Virtual Network."
+}
+
+output "subnet_name" {
+  value       = azurerm_subnet.subnet.name
+  description = "The name of the Subnet."
 }
